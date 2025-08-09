e4bd4797024199e762e97cec70bbdaea26f0a992
Alvis <aherrera@munily.com>
2025-08-08 23:46:08 -0500

    feat: Add cache headers to root endpoint for improved performance
    - Implement cache control headers to the root endpoint, setting a max-age of 24 hours to enhance response efficiency and reduce server load.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ import airdropRoutes from './routes/airdrop';
 // Rutas
 app.get('/', (req, res) => {
   console.log('Solicitud recibida en /social-drops-metadata');
+
+  // Headers de cache de 24 horas
+  const oneDayInSeconds = 24 * 60 * 60; // 24 horas en segundos
+
+  res.set({
+    'Cache-Control': `public, max-age=${oneDayInSeconds}`,
+    Expires: new Date(Date.now() + oneDayInSeconds * 1000).toUTCString(),
+  });
+
   res.json(socialDrops);
 });
 
